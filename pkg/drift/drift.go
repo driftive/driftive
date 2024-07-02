0a27be96474f38c79f5321ed99b20808e1da53d7
Fernando Nogueira <fernandonogueira@users.noreply.github.com>
2024-07-01 22:29:12 -0300

    fix: drift detection message

--- pkg/drift/drift.go
+++ pkg/drift/drift.go
@@ -53,7 +53,9 @@ func (d *DriftDetector) detectDriftConcurrently(dir string, projectDir string) {
 	if err != nil {
 		log.Info().Msgf("Error checking drift in %s: %v", dir, err)
 	}
-	log.Info().Msgf("Drift detected in project %s", projectDir)
+	if result {
+		log.Info().Msgf("Drift detected in project %s", projectDir)
+	}
 	d.results <- DriftProjectResult{Project: projectDir, Drifted: result}
 }
 
