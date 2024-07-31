60e0689b74336f07c19c6848fdf5105094eb6e2e
Fernando Nogueira <fernandonogueira@users.noreply.github.com>
2024-07-30 21:55:33 -0300

    chore: include project type in logs (tf/tg/tofu)

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -13,3 +13,16 @@ type Project struct {
 	Dir  string      `json:"dir" yaml:"dir"`
 	Type ProjectType `json:"type" yaml:"type"`
 }
+
+func ProjectTypeToStr(t ProjectType) string {
+	switch t {
+	case Terraform:
+		return "tf"
+	case Tofu:
+		return "tofu"
+	case Terragrunt:
+		return "tg"
+	default:
+		return "?"
+	}
+}
