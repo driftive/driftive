b62459429160dcfdb63181a0c31d967d50de5b78
Fernando Nogueira <fernandonogueira@users.noreply.github.com>
2024-08-30 21:44:08 -0300

    feat: add support for creating a summary issue

---
+++ pkg/summary/github/gh_summary_test.go
@@ -0,0 +1,26 @@
+package github
+
+import (
+	_ "embed"
+	"strings"
+	"testing"
+)
+
+//go:embed tests/expected_summary.md
+var expected string
+
+func TestGetSummaryIssueBody(t *testing.T) {
+	summary := GithubSummary{
+		DriftedProjects:     []string{"projs/project1", "projs/project2"},
+		ErroredProjects:     []string{"projs/project3"},
+		RateLimitedProjects: []string{"projs/project4"},
+		LastAnalysisDate:    "2021-08-01T03:32:12Z",
+	}
+	result, err := getSummaryIssueBody(summary)
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
+	if strings.Trim(*result, " \n") != strings.Trim(expected, " \n") {
+		t.Errorf("\nExpected\n-----\n%s\n-----, got -----\n%s\n-----", expected, *result)
+	}
+}
---
+++ pkg/summary/github/tests/expected_summary.md
@@ -0,0 +1,20 @@
+This issue shows your drift summary.
+
+## Rate-Limited
+
+These issues are currently rate-limited.
+* `projs/project4`
+## Drifts
+
+These issues show the drifts between the desired and actual state of your resources.
+* `projs/project1`
+* `projs/project2`
+## Errors
+
+These issues show the errors that occurred during the drift analysis.
+* `projs/project3`
+<!--
+summary-state-start
+{"rate_limited_projects":["projs/project4"],"drifted_projects":["projs/project1","projs/project2"],"errored_projects":["projs/project3"],"last_analysis_date":"2021-08-01T03:32:12Z"}
+summary-state-end
+-->
