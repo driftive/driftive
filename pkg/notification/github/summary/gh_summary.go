b8453eab72ebda46ef2d6d1b2733305dfd91f72b
Fernando Nogueira <fernandonogueira@users.noreply.github.com>
2025-06-28 23:12:43 -0300

    chore(deps): upgrade go-github from v72 to v73

--- pkg/notification/github/summary/gh_summary.go
+++ pkg/notification/github/summary/gh_summary.go
@@ -10,7 +10,7 @@ import (
 	_ "embed"
 	"encoding/json"
 	"fmt"
-	"github.com/google/go-github/v72/github"
+	"github.com/google/go-github/v73/github"
 	"github.com/rs/zerolog/log"
 	"strings"
 	"text/template"
